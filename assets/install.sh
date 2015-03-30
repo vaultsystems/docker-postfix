f4cf16b4ea97173c147ba185481088d27aa18cc5
Christoph Dwertmann <cdwertmann@gmail.com>
2015-03-30 17:58:49 +1100

    increase message size

--- assets/install.sh
+++ assets/install.sh
@@ -29,6 +29,7 @@ chmod +x /opt/postfix.sh
 postconf -e myhostname=$mydestination
 postconf -F '*/*/chroot = n'
 postconf -e inet_protocols=ipv4
+postconf -e message_size_limit=20480000
 postconf -e 'home_mailbox=Maildir/'
 postconf -e 'smtpd_banner=$myhostname Microsoft ESMTP MAIL Service, Version: 5.0.2195.1600 ready'
 postconf -e disable_vrfy_command=yes
